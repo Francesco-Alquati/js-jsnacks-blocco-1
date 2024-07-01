d768f3fbb686d6e666f431b8f6199c3c037a1cea
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-07-01 18:56:00 +0200

    [UPD] Created count variable and get it from DOM

--- jsnack2/script.js
+++ jsnack2/script.js
@@ -4,6 +4,12 @@ let array = [];
 // RICHIAMO IL BOTTONE DAL DOM
 const btn = document.getElementById('button');
 
+// CREO UNA VARIABILE CONTATORE
+let count = 0;
 
+// RICHIAMO IL CONTATORE DAL DOM
+let disp = document.getElementById("display");
+
+    
 
 
