1f8bc87406ea8f923b4b9f344f731ed03c555208
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-07-01 18:21:48 +0200

    [UPD] Get the button from DOM

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -1,4 +1,9 @@
 // CREO IL MIO ARRAY DI EMAIL
 const invited = ["Francesco", "Francesca", "Mario", "Maria", "Daniele", "Daniela", "Leonardo", "Riccardo"];
 
+// RECUPERO IL PULSANTE DAL DOM
+const button = document.getElementById('search');
 
+button.addEventListener('click', function (){
+
+})
