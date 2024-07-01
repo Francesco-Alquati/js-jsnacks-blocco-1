dfc4e1167ac3f107ab5e453a79b9a45c339caba6
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-07-01 19:56:59 +0200

    [UPD] Get button from DOM and added click function

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -1,3 +1,10 @@
 // CREO UNA VARIABILE PER LA SOMMA
 let somma = 0;
 
+// RICHIAMO IL CONTATORE DAL DOM
+let btn = document.getElementById("calculate");
+
+// APPLICO UNA FUNZIONE AL BOTTONE
+btn.addEventListener('click' ,function (){
+
+})
