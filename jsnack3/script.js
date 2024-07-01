e339d896a444e25ea0bb20e04e68f6b25f069bbd
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-07-01 20:01:45 +0200

    [UPD] Created array with split method

--- jsnack3/script.js
+++ jsnack3/script.js
@@ -7,4 +7,11 @@ let btn = document.getElementById("calculate");
 // APPLICO UNA FUNZIONE AL BOTTONE
 btn.addEventListener('click' ,function (){
 
+    // RICHIAMO L'INPUT DAL DOM
+    let number = document.getElementById('number').value;
+    
+    // CREO L'ARRAY
+    const array = number.split("");
+    console.log(array);
+    
 })
