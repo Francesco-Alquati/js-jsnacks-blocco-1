38079bd05ba8dd68ca04da99e8dee42281381ec9
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-07-01 18:23:51 +0200

    [UPD] Get imput value from DOM

--- jsnack1/script.js
+++ jsnack1/script.js
@@ -6,4 +6,6 @@ const button = document.getElementById('search');
 
 button.addEventListener('click', function (){
 
+    // RECUPERO IL VALORE INSERITO NEL CAMPO INPUT
+    let name = document.getElementById('user_name').value;
 })
